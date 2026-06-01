0b537ae15b8a0665b8174ca8657a441a490f9c8d
ElApando <jrbartolomeo@comunidad.unam.mx>
2026-06-01 17:07:23 -0600

    Se agregan 182, 183, 196, 197

--- 171_excel_sheet_column_number.py
+++ 171_excel_sheet_column_number.py
@@ -1,6 +1,7 @@
 """ 171. Excel Sheet Column Number
 
-Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+Given a string columnTitle that represents the column title as appears in an Excel sheet, return its 
+corresponding column number.
 
 For example:
 
@@ -28,9 +29,9 @@ Output: 701
 
 from typing import Dict, List
 
-di_refs: Dict[str, int] = { "A": 1, "B":2, "C":3, "D":4, "E":5, "F":6, "G":7, "H":8, "I":9,
-    "J":10, "K":11, "L":12, "M":13, "N":14, "O":15, "P":16, "Q":17, "R":18, "S":19, "T":20,
-    "U":21, "V":22, "W":23, "X":24, "Y":25, "Z":26}
+di_refs: Dict[int, str] = { 1:"A", 2:"B", 3:"C", 4:"D", 5:"E", 6:"F", 7:"G", 8:"H", 9:"I",
+    10: "J", 11:"K", 12:"L", 13:"M", 14:"N", 15:"O", 16:"P", 17:"Q", 18:"R", 19:"S", 20:"T",
+    21: "U", 22:"V", 23:"W", 24:"X", 25:"Y", 26:"Z"}
 
 ls_prove: List[int] = [1, 28, 701, 2147483647]
 
