518a40649af5c9293869ca5f4711a20a94b30b38
ElApando <jrbartolomeo@comunidad.unam.mx>
2026-06-05 18:41:06 -0600

    Se agregan 602, 619, 627, 1050

--- 607_sales_person.sql
+++ 607_sales_person.sql
@@ -96,6 +96,7 @@
 
 SELECT  name
 FROM SalesPerson
-WHERE sales_id NOT IN ( SELECT sales_id FROM Orders o JOIN Company c ON o.com_id = c.com_id WHERE c.name = 'RED')
+WHERE sales_id NOT IN ( SELECT sales_id FROM Orders o 
+JOIN Company c ON o.com_id = c.com_id WHERE c.name = 'RED')
 
 -- Finite Incantatem
