f4bb429ae7f5be2cc407686ca09ea96f56bc91f2
ElApando <jrbartolomeo@comunidad.unam.mx>
2026-06-26 18:08:57 -0600

    Se ordena la carpeta y se prepara para el Readme

--- configuration/config.py
+++ configuration/config.py
@@ -1,5 +1,4 @@
-""" 
-DOC
+"""  Configuración de Rutas
 """
 
 di_routes = {"595":"refs/595.txt",
@@ -10,5 +9,22 @@ di_routes = {"595":"refs/595.txt",
              "1068_2": "refs/1068_2.txt",
              "197": "refs/197.txt",
              "1148": "refs/1148.txt"}
+
+import os
+from pathlib import Path
+
+pa_path_origin = Path("C:/Users/DELL/Respaldo/DON VILLA NUEVO/UNIVERSITY/Proyecto/PYTHON/GitHub/LeetCode/")
+ls_files = os.listdir(pa_path_origin)
+
+for file in ls_files:
+    pa_path = pa_path_origin / file
+
+    if ".sql" in file:
+        pa_path_sql = Path(f"{pa_path_origin}/exercice_sql/{file}")
+        pa_path.rename(pa_path_sql)
+
+    if ".py" in file:
+        pa_path_py = Path(f"{pa_path_origin}/exercice_python/{file}")
+        pa_path.rename(pa_path_py)
 
 # Finite Incantatem
